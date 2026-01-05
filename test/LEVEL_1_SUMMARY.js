7604fde763c4e65f9f4521d8f314151065e5ba3e
Odavl <contact@odavl.com>
2026-01-05 18:25:26 +0100

    Delete test directory

--- test/LEVEL_1_SUMMARY.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-// DEPRECATED: This file has been moved to ./internal and is not a test.
-console.error('LEVEL_1_SUMMARY.js has moved to ./internal and should not be used.');
-process.exit(1);
